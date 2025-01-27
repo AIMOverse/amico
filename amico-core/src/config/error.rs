7397df1c549f30e7ec93be4af48c9d4cfe01b7c8
takasaki404 <takasaki@aimoverse.xyz>
2025-01-27 09:34:31 +0800

    using `WithParams` macro for config with params

--- amico-core/src/config/error.rs
+++ amico-core/src/config/error.rs
@@ -2,7 +2,4 @@
 pub enum ConfigError {
     #[error("Failed to load config")]
     FailedToLoadToml(#[from] toml::de::Error),
-
-    #[error("Param {0} not found")]
-    ParamNotFound(String),
 }
--- amico-core/src/config/event.rs
+++ amico-core/src/config/event.rs
@@ -1,23 +1,12 @@
 use serde::{Deserialize, Serialize};
 
-use super::{params::Params, ConfigError, ParamValue};
+use super::params::{Params, WithParams};
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, WithParams)]
 #[serde(rename_all = "snake_case")]
 pub struct EventConfig {
     pub name: String,
     pub source: String,
+    #[params]
     pub params: Option<Params>,
-}
-
-impl EventConfig {
-    pub fn param(&self, key: &str) -> Result<&ParamValue, ConfigError> {
-        let Some(params) = self.params.as_ref() else {
-            return Err(ConfigError::ParamNotFound(key.to_string()));
-        };
-
-        params
-            .get(key)
-            .ok_or(ConfigError::ParamNotFound(key.to_string()))
-    }
 }
