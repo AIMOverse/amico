5e3447ed81975ab180d29204f7d8d7b47eb42b1b
Yue Pan <79363355+dcloud347@users.noreply.github.com>
2025-02-01 17:38:11 +0000

    refactor: update documentation for event generation and action selection traits

--- amico-core/src/traits/action_selector.rs
+++ amico-core/src/traits/action_selector.rs
@@ -4,5 +4,10 @@ use crate::traits::Action;
 /// Trait for selecting an action based on an event.
 pub trait ActionSelector {
     /// Selects an action based on the given event.
+    /// Arguments:
+    /// * `events` - A vector of Event instances in the current event pool.
+    /// Returns:
+    /// * A tuple containing the selected action
+    /// and the IDs of the events that is going to be removed from event pool.
     fn select_action(&self, events: Vec<Event>) -> (Box<dyn Action>, Vec<u32>);
 }
