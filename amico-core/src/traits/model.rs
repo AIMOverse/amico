445124be10f6161c05638a09be8367ca1cbb9302
Yue Pan <79363355+dcloud347@users.noreply.github.com>
2025-02-01 16:44:02 +0000

    refactor: enhance event processing by optimizing data structures and improving performance

--- amico-core/src/traits/model.rs
+++
@@ -1,8 +0,0 @@
-/// Trait representing a model that can predict and sense the environment.
-pub trait Model {
-    /// Predicts the environment.
-    fn predict_environment(&self);
-
-    /// Perceive the environment.
-    fn perceive_environment(&self);
-}
