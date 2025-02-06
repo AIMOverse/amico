fa8ac876cbb3406eb524379e2a8b31fcec235955
takasaki404 <takasaki@aimoverse.xyz>
2025-02-06 21:39:13 +0800

    remove `amico-std` crate

--- amico-core/src/lib.rs
+++ amico-core/src/lib.rs
@@ -1,11 +1,6 @@
 pub mod config;
 pub mod controller;
+pub mod entities;
+pub mod errors;
 pub mod plugin;
 pub mod traits;
-
-pub mod entities;
-
-pub mod errors;
-
-#[cfg(feature = "std")]
-pub mod std;
