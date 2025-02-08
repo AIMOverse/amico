4bbcb1df07ee5e1e9276c30ddaf1bc5f3c044be0
takasaki404 <takasaki@aimoverse.xyz>
2025-02-08 19:32:18 +0800

    feat: openai provider using `rig-core`

---
+++ amico-plugins/src/interface/provider.rs
@@ -0,0 +1,5 @@
+use amico::ai::provider::Provider;
+
+/// The trait for providers of AI models.
+/// This plugin should implement the `Provider` trait from the `amico-sdk` crate.
+pub trait ProviderPlugin: Provider {}
