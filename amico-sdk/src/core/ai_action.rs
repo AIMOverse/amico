4133f775223c36e14b48d70dfdfc93887e9f3e68
Yue Pan <79363355+dcloud347@users.noreply.github.com>
2025-02-13 22:38:30 +0000

    fix: update action closure parameter to pass by value for improved performance

--- amico-sdk/src/core/ai_action.rs
+++ amico-sdk/src/core/ai_action.rs
@@ -5,12 +5,12 @@ pub struct AIAction {
     pub description: String,                       // The description of the action
     pub parameters: serde_json::Value,             // The actual parameters
     pub parameters_description: serde_json::Value, // The description of the parameters (The types of the parameters)
-    pub action: Box<dyn Fn(&serde_json::Value) -> Result<(), ActionError>>, // The actual action
+    pub action: Box<dyn Fn(serde_json::Value) -> Result<(), ActionError>>, // The actual action
 }
 
 impl amico_core::traits::Action for AIAction {
     // Execute the action
     fn execute(&self) -> Result<(), ActionError> {
-        (self.action)(&self.parameters)
+        (self.action)(self.parameters.clone())
     }
 }
