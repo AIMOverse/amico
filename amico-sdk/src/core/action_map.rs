26015bd000c9f3c59346dad150288c3d90b3c939
Yue Pan <79363355+dcloud347@users.noreply.github.com>
2025-02-21 16:01:22 +0000

    feat: improve documentation in ActionMap and Model traits for better clarity

--- amico-sdk/src/core/action_map.rs
+++ amico-sdk/src/core/action_map.rs
@@ -1,11 +1,13 @@
 use crate::core::ai_action::AIAction;
 use std::collections::HashMap;
 
+/// A map of actions that can be understood by AI.
 #[derive(Default)]
 pub struct ActionMap {
     actions: HashMap<String, AIAction>,
 }
 
+/// Implementation of the ActionMap struct.
 impl ActionMap {
     pub fn new() -> Self {
         Self {
@@ -13,16 +15,21 @@ impl ActionMap {
         }
     }
 
+    /// Adds an action to the map.
     pub fn add_action(&mut self, action: AIAction) {
         self.actions.insert(action.name.clone(), action);
     }
 
+    /// Get a copy of the action from the map.
     pub fn get(&self, name: &str) -> Option<AIAction> {
         self.actions.get(name).cloned()
     }
 
+    /// Get a description of the actions in the map.
     pub fn describe(&self) -> String {
+        // Create a string to store the result
         let mut result = String::new();
+        // Iterate over the actions in the map
         for (name, action) in &self.actions {
             result.push_str(&format!(
                 "name: {} \n\
