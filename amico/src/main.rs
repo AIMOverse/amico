1a7f5238c9f805616c3f8d77598e83ee33fd7cfc
ZaneArcher <fracher21@gmail.com>
2025-05-10 13:59:58 +0800

    A2A integration (#96)
    Co-authored-by: Archer <archer@aimoverse.xyz>

--- amico/src/main.rs
+++ amico/src/main.rs
@@ -1,8 +1,10 @@
 use std::process;
+use std::sync::Arc;
 
 use amico::ai::services::ServiceBuilder;
 use amico::resource::Resource;
 use amico_mods::interface::Plugin;
+use amico_mods::runtime::storage::fs::FsStorage;
 use amico_mods::std::ai::providers::rig::{providers, RigProvider};
 use amico_mods::std::ai::services::InMemoryService;
 use amico_mods::web3::solana::std::balance::BalanceSensor;
@@ -10,6 +12,7 @@ use amico_mods::web3::solana::std::client::{SolanaClient, SolanaClientResource};
 use amico_mods::web3::solana::std::trade::TradeEffector;
 use amico_mods::web3::wallet::Wallet;
 use colored::Colorize;
+use engine::a2a::A2aModule;
 use engine::agent::Agent;
 use engine::components::{AiService, Recorder};
 use engine::interaction::Stdio;
@@ -17,6 +20,7 @@ use engine::systems::{ChatbotSystem, CompletionSystem, SpeechSystem};
 use evenio::prelude::*;
 use helpers::solana_rpc_url;
 use prompt::AMICO_SYSTEM_PROMPT;
+use tokio::sync::Mutex;
 
 mod audio;
 mod engine;
@@ -69,6 +73,16 @@ async fn main() {
         println!();
     }
 
+    let fs_store = FsStorage::new(".amico/storage")
+        .inspect_err(|err| {
+            eprintln!("{}", "Error loading FS storage".red());
+            tracing::error!("Error loading FS storage: {}", err);
+            process::exit(1);
+        })
+        .unwrap();
+    let storage_resource =
+        Resource::new("Dev FS Storage".to_string(), Arc::new(Mutex::new(fs_store)));
+
     // Load agent wallet
     let wallet = Wallet::load_or_save_new("agent_wallet.txt")
         .inspect(|_| println!("{}", "✔ Loaded agent wallet".green()))
@@ -97,6 +111,16 @@ async fn main() {
         TradeEffector::new(client.clone(), wallet.clone()),
     );
 
+    // Initialize a2a module
+    let a2a = A2aModule::new(wallet.clone(), storage_resource);
+
+    // Connect to the network
+    if let Err(e) = a2a.connect().await {
+        tracing::error!("{}", e);
+        eprintln!("Failed to connect to a2a network");
+        process::exit(1);
+    }
+
     // Create the Provider
     let provider = RigProvider::openai(providers::openai::Client::from_url(
         &openai_api_key,
@@ -112,6 +136,8 @@ async fn main() {
         .tool(balance_sensor.value().agent_wallet_balance_tool())
         .tool(balance_sensor.value().account_balance_tool())
         .tool(trade_effector.value().tool())
+        .tool(a2a.send_message_tool())
+        .tool(a2a.contact_list_tool())
         .build::<InMemoryService<RigProvider>>();
 
     println!();
@@ -148,7 +174,7 @@ async fn main() {
     speech.register_to(&mut world);
     chatbot.register_to(&mut world);
 
-    let agent = Agent::new(&mut world, itr_layer);
+    let agent = Agent::new(&mut world, itr_layer, a2a);
     if let Err(e) = agent.run().await {
         tracing::error!("{e}");
     }
