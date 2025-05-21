c62438589b2dfa479dbd55b8af96334462317e6c
Archer <archer@aimoverse.xyz>
2025-05-22 02:26:32 +0800

    add handler docs

--- amico-core/src/traits/handlers.rs
+++ amico-core/src/traits/handlers.rs
@@ -5,11 +5,68 @@ use crate::ecs;
 
 use super::System;
 
+/// The Observer trait is used to observe events.
+///
+/// Observers watch for an event and process it, without
+/// modifying the event and affecting the state of the world.
+pub trait Observer {
+    /// The event type that the Observer observes.
+    type Event: ecs::GlobalEvent + 'static;
+
+    /// The observe method is called when the Observer is notified of an event.
+    ///
+    /// The method receives a reference to the event and returns a Result.
+    fn observe(&self, event: &<Self::Event as ecs::Event>::This<'_>) -> Result<()>;
+
+    /// Converts the Observer into a SystemHandler.
+    fn to_system(self) -> SystemHandler<Self, PhantomMediator>
+    where
+        Self: Sized,
+    {
+        SystemHandler::Observer(self)
+    }
+}
+
+/// The Mediator trait is used to mediate events.
+///
+/// Mediators watch for an event and process it, and finally
+/// take ownership of the event.
+///
+/// After processing, Mediators may send new events to the world.
+pub trait Mediator {
+    /// The event type that the Mediator mediates.
+    type Event: ecs::GlobalEvent + ecs::Event<Mutability = ecs::Mutable>;
+
+    /// The event type that the Mediator sends.
+    type EventsToSend: ecs::EventSet;
+
+    /// The mediate method is called when the Mediator is notified of an event.
+    ///
+    /// The method receives a mutable reference to the event and a sender to send
+    /// new events to the world.
+    fn mediate(
+        &self,
+        event: &mut EventMut<'_, Self::Event>,
+        sender: ecs::Sender<Self::EventsToSend>,
+    ) -> Result<()>;
+
+    /// Converts the Mediator into a SystemHandler.
+    fn to_system(self) -> SystemHandler<PhantomObserver, Self>
+    where
+        Self: Sized,
+    {
+        SystemHandler::Mediator(self)
+    }
+}
+
+/// The placeholder event for the observer and mediator to use in SystemHandler.
 #[derive(ecs::GlobalEvent)]
 pub struct PhantomEvent;
 
+/// The placeholder observer for SystemHandler.
 pub struct PhantomObserver;
 
+/// The placeholder mediator for SystemHandler.
 pub struct PhantomMediator;
 
 impl Observer for PhantomObserver {
@@ -33,43 +90,16 @@ impl Mediator for PhantomMediator {
     }
 }
 
+/// The SystemHandler is a middleware to convert an Observer or Mediator into a System.
 pub enum SystemHandler<
     O: Observer + 'static = PhantomObserver,
     M: Mediator + 'static = PhantomMediator,
 > {
+    /// The Observer handler variant.
     Observer(O),
+
+    /// The Mediator handler variant.
     Mediator(M),
-}
-
-pub trait Observer {
-    type Event: ecs::GlobalEvent + 'static;
-
-    fn observe(&self, event: &<Self::Event as ecs::Event>::This<'_>) -> Result<()>;
-
-    fn to_system(self) -> SystemHandler<Self, PhantomMediator>
-    where
-        Self: Sized,
-    {
-        SystemHandler::Observer(self)
-    }
-}
-
-pub trait Mediator {
-    type Event: ecs::GlobalEvent + ecs::Event<Mutability = ecs::Mutable>;
-    type EventsToSend: ecs::EventSet;
-
-    fn mediate(
-        &self,
-        event: &mut EventMut<'_, Self::Event>,
-        sender: ecs::Sender<Self::EventsToSend>,
-    ) -> Result<()>;
-
-    fn to_system(self) -> SystemHandler<PhantomObserver, Self>
-    where
-        Self: Sized,
-    {
-        SystemHandler::Mediator(self)
-    }
 }
 
 impl<O, M> System for SystemHandler<O, M>
