e8104546748ce04b505583870b97668e7521fec2
Archer <archer@aimoverse.xyz>
2025-05-25 07:33:01 +0800

    add resource map

---
+++ amico-core/src/traits/resource.rs
@@ -0,0 +1,4 @@
+/// A trait for resources.
+///
+/// This is an empty trait that can be used to tag resources.
+pub trait Resource: 'static {}
--- amico-core/src/types/mod.rs
+++ amico-core/src/types/mod.rs
@@ -1,7 +1,9 @@
 mod agent_event;
 mod event_pool;
 mod instruction;
+mod res_map;
 
 pub use agent_event::*;
 pub use event_pool::*;
 pub use instruction::*;
+pub(crate) use res_map::*;
