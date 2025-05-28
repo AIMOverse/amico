7d4a1c5848bb70938ec6e44443845a384ee98254
Archer <fracher21@gmail.com>
2025-05-29 05:53:32 +0800

    Refactor amico SDK (#105)
    Co-authored-by: Archer <archer@aimoverse.xyz>

--- amico-mods/src/std/ai/services/mod.rs
+++ amico-mods/src/std/ai/services/mod.rs
@@ -2,3 +2,5 @@
 mod in_memory;
 #[cfg(feature = "std-services-in-memory")]
 pub use in_memory::*;
+
+pub mod speech;
--- amico-sdk/src/lib.rs
+++ amico-sdk/src/lib.rs
@@ -1,11 +1,6 @@
 pub mod ai;
 pub mod environment;
-pub mod interaction;
 pub mod resource;
-pub mod task;
 
 #[cfg(feature = "a2a")]
 pub mod a2a;
-
-#[cfg(feature = "aoe")]
-pub mod aoe;
