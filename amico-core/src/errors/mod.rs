28df97008e46a6cc69e4e1248ab34616ceffcd31
Archer <archer@aimoverse.xyz>
2025-05-29 14:27:21 +0800

    remove unused errors

--- amico-core/src/errors/mod.rs
+++ amico-core/src/errors/mod.rs
@@ -1,9 +1,5 @@
-mod action_errors;
-mod action_selector_errors;
 mod agent_event;
-mod event_pool_errors;
+mod event_pool;
 
-pub use action_errors::*;
-pub use action_selector_errors::*;
 pub use agent_event::*;
-pub use event_pool_errors::*;
+pub use event_pool::*;
