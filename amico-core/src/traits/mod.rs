75767268fd3805d5f8b092787e48dd026ce361ee
Archer <fracher21@gmail.com>
2025-06-03 16:58:16 +0800

    Remove `Resource` trait (#111)
    Co-authored-by: Archer <archer@aimoverse.xyz>

--- amico-core/src/traits/mod.rs
+++ amico-core/src/traits/mod.rs
@@ -1,11 +1,9 @@
 mod dispatcher;
 mod event_source;
 mod handlers;
-mod resource;
 mod system;
 
 pub use dispatcher::*;
 pub use event_source::*;
 pub use handlers::*;
-pub use resource::*;
 pub use system::*;
